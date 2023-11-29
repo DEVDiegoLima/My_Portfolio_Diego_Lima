e821626ab3cf05b28adbf97d0f2809ad5da9b535
Diego Lima <diegogoncalveisreisdelima@gmail.com>
2023-11-29 07:40:39 -0300

    atualização 1

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,23 +6,32 @@ export const Contact = () => {
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
         <h2>Contato</h2>
-        <p>Sinta-se á vontade <br/>para entra em contato!</p>
+        <p>
+          Sinta-se á vontade <br />
+          para entra em contato!
+        </p>
       </div>
       <ul className={styles.links}>
-        <li className={styles.link}>
-          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:diegogoncalvesreisdelima21@gmail.com">diegogoncalvesreisdelima21@gmail.com</a>
-        </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/in/diego-lima-a3aa81253/">linkedin.com/Diego Lima</a>
+          <a href="https://www.linkedin.com/in/diego-lima-a3aa81253/">
+            linkedin.com/Diego Lima
+          </a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/DEVDiegoLima">github.com/DEVDiegoLima</a>
+          <a href="https://www.github.com/DEVDiegoLima">
+            github.com/DEVDiegoLima
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:diegogoncalvesreisdelima21@gmail.com">
+            diegogoncalvesreisdelima21@gmail.com
+          </a>
         </li>
       </ul>
     </footer>
